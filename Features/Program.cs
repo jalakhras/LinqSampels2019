838dfaf526c592f645572d0a04ba9a68890be675
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-11-16 17:31:21 +0300

    Using Func and Action Types

--- Features/Program.cs
+++ Features/Program.cs
@@ -7,6 +7,21 @@ namespace Features
     {
         static void Main(string[] args)
         {
+
+            Func<int, int> sequre = sequreFunction;
+            Func<int, int> sequre2 = number => number * number;
+            Func<int, int, int> Add = (x, y) => x + y;
+            Action<string> write = x => Console.WriteLine(x);
+
+            Action<string,string> writeFullName =(FName,LName) => Console.WriteLine($"Full Name is : {FName} {LName}");
+
+            Console.WriteLine(sequre(10));
+            Console.WriteLine(sequre2(10));
+            Console.WriteLine(sequre(Add(10,20)));
+            write("called Action");
+
+            writeFullName("Jassar", "Mahmoud");
+
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id=1,Name="Jassar"},
@@ -24,6 +39,11 @@ namespace Features
             {
                 Console.WriteLine(enumerator.Current.Name);
             }
+        }
+
+        private static int sequreFunction(int arg)
+        {
+            return arg * arg;
         }
     }
 }

7fffc0ab4b5592d44c654901114f8d404d2d69e7
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-11-18 14:35:24 +0300

    Read data from cvc file and using Linq

---
+++ Car/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProsseceFile("fuel.csv");
+            cars.ForEach(car =>
+            {
+                Console.WriteLine(car.Name);
+
+            });
+        }
+
+        private static List<Car> ProsseceFile(string path)
+        {
+            var Data = File.ReadAllLines(path).Skip(1).Where(line => line.Length > 1)
+                .Select(line => Car.ParseFromCvc(line)).ToList();
+            return Data;
+        }
+
+
+    }
+}
